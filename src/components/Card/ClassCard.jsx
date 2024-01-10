63f5d0b5e708f9254b50f3cca3c722c55833756f
Emon <emonhasan7650@gmail.com>
2024-01-10 16:29:15 +0600

    all class page redesign

--- src/components/Card/ClassCard.jsx
+++ src/components/Card/ClassCard.jsx
@@ -5,61 +5,72 @@ import Swal from "sweetalert2";
 import { useTheme } from "../../providers/ThemeProvider";
 
 const ClassCard = ({ classData }) => {
-    const {user, role} = useContext(AuthContext);
-    const [disabled, setDisabled] = useState(false);
-    const { theme } = useTheme(); // for using light and dark themes
+  const { user, role } = useContext(AuthContext);
+  const [disabled, setDisabled] = useState(false);
+  const { theme } = useTheme(); // for using light and dark themes
 
-    const {className, image, instructorName, seats, price} = classData;
-    // save class in db
-    const saveClassToDb = () => {
-
-        const classData = {
-            name: user?.displayName,
-            userImg: user?.photoURL,
-            email: user?.email,
-            className,
-            image,
-            instructorName,
-            seats,
-            price
+  const { className, image, instructorName, seats, price } = classData;
+  // save class in db
+  const saveClassToDb = () => {
+    const classData = {
+      name: user?.displayName,
+      userImg: user?.photoURL,
+      email: user?.email,
+      className,
+      image,
+      instructorName,
+      seats,
+      price,
+    };
+    selectClass(classData)
+      .then((data) => {
+        console.log(data);
+        setDisabled(true);
+        if (data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Class added successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
         }
-        selectClass(classData)
-        .then(data => {
-            console.log(data); 
-            setDisabled(true);
-         if (data.insertedId) {
-            Swal.fire({
-                position: "top-end",
-                icon: "success",
-                title: "Class added successfully",
-                showConfirmButton: false,
-                timer: 1500,
-              });
-
-         }
-        })
-        .catch(err => {
-          console.log(err);
-        })
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="card w-full bg-base-100">
+    <div className="w-full bg-base-100">
       <figure>
         <img
-        className="h-[280px] md:h-[320px] rounded-md"
+          className="w-full sm:h-[240px] md:h-[210px] rounded-sm hover:scale-105 duration-[1500ms]"
           src={image}
           alt="Class Image"
         />
       </figure>
-      <div className={`card-body ${theme.mode=== 'dark'? 'text-gray-100' : 'text-gray-800'} grid place-items-center`}>
-        <h2 className="card-title">{className}</h2>
-        <p className="font-semibold">Instructor: {instructorName}</p>
-        
-            <p className="text-sm -my-[2px] font-medium">Available Seats: {seats}</p>
-            <p className="text-sm -my-[2px] font-medium">Course Free: ${price}</p>
-       
-        <div className="card-actions justify-center mt-2">
-          <button onClick={saveClassToDb} className="btn btn-sm" disabled={role === 'admin' || role === 'instructor' || disabled}>Select Class</button>
+      <div
+        className={`mt-3 sm:mt-4${
+          theme.mode === "dark" ? "text-gray-100" : "text-gray-800"
+        }`}
+      >
+        <h2 className="text-sm sm:text-base font-semibold">{className}</h2>
+        <div className="text-xs sm:text-sm capitalize mt-1">
+        <p>Instructor: {instructorName}</p>
+        <p>
+          Seats: {seats}
+        </p>
+        <p>Course Free: ${price}</p>
+        </div>
+
+        <div className="mt-1">
+          <button
+            onClick={saveClassToDb}
+            className="btn btn-xs"
+            disabled={role === "admin" || role === "instructor" || disabled}
+          >
+            Select Class
+          </button>
         </div>
       </div>
     </div>
--- src/pages/AllClass/AllClass.jsx
+++ src/pages/AllClass/AllClass.jsx
@@ -13,16 +13,18 @@ const AllClass = () => {
       <Helmet>
         <title>Artistry Academia | All Classes</title>
       </Helmet>
-      <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {loading &&
-          Array.from(new Array(6)).map((item, index) => (
-            <CardSkeleton key={index} height={300} />
-          ))}
-        {!loading &&
-          approveClass.length > 0 &&
-          approveClass.map((classData) => (
-            <ClassCard key={classData._id} classData={classData} />
-          ))}
+      <div className="mt-16 mb-10">
+        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {loading &&
+            Array.from(new Array(8)).map((item, index) => (
+              <CardSkeleton key={index} height={300} />
+            ))}
+          {!loading &&
+            approveClass.length > 0 &&
+            approveClass.map((classData) => (
+              <ClassCard key={classData._id} classData={classData} />
+            ))}
+        </div>
       </div>
     </Container>
   );
